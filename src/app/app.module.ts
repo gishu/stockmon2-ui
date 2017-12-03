991717edbb72b5d70730673402fd41f55d3d5e6c
Gishu Pillai <gishu@hotmail.com>
2017-12-03 23:03:31 +0530

    Declare single route to new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { TradesComponent } from './trades/trades.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TradesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'trades', component: TradesComponent },
+      { path: '', redirectTo: 'trades', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
